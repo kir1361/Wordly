506ecd2fa713634f93f0b97abf8af0e30ebaf682
kir1361 <dovbnya.kirill@gmail.com>
2024-06-30 16:56:56 +0300

    Wordly

---
+++ Cursova/Source.cpp
@@ -0,0 +1,528 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <vector>
+#include <locale>
+#include <codecvt>
+#include <Lmcons.h>
+#include <ctime>
+#include "resource1.h"
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+using namespace std;
+
+
+// Глобальні змінні:
+HINSTANCE hInst; 	//Дескриптор програми	
+LPCTSTR szWindowClass = L"QWERTY";
+LPCTSTR szTitle = L"Crossword";
+vector<wstring> EnteredWord;
+wstring SecretWord;
+bool allCellsFilled = true;
+COLORREF color = RGB(255, 255, 255);
+HWND cells[6][5];
+int currentRow = 0;
+int currentCell = 0;
+int mistakesCount = 0;
+int enteredWordCount = 0;
+
+HFONT CreateCustomFont(int size)
+{
+	return CreateFont
+	(
+		size, 0, 0, 0, FW_BOLD, FALSE, FALSE, FALSE, UNICODE,
+		OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY,
+		DEFAULT_PITCH | FF_SWISS, L"TimesNewRoman"
+	);
+}
+HFONT hFont = CreateCustomFont(40);
+
+void ApplyCustomFontToAllEditBoxes(HFONT hFont, HWND cells[6][5]) {
+	for (int row = 0; row < 6; row++) {
+		for (int col = 0; col < 5; col++) {
+			HWND hEditBox = cells[row][col];
+			SendMessage(hEditBox, WM_SETFONT, (WPARAM)hFont, TRUE);
+		}
+	}
+}
+
+void InitializeEditBoxes(HWND hwndParent) {
+	for (int row = 0; row < 6; row++) {
+		for (int col = 0; col < 5; col++) {
+			int id = 1061 + row * 5 + col;
+
+			cells[row][col] = GetDlgItem(hwndParent, id);
+
+
+		}
+	}
+}
+
+
+vector<wstring> loadWordsFromFile(const string& filename) {
+	vector<wstring> words;
+	wifstream file(filename);
+
+	file.imbue(locale(file.getloc(), new codecvt_utf8<wchar_t, 0x10ffff, codecvt_mode::little_endian>));
+
+
+	if (file.is_open()) {
+		wstring word;	
+		while (getline(file, word)) {
+			words.push_back(word);
+		}
+		file.close();
+	}
+	else {
+		cerr << "Error" << filename << endl;
+	}
+
+	return words;
+}
+
+wstring chooseRandomWord(const vector<wstring>& words) {
+	if (words.empty()) {
+		cerr << "No words available to choose from." << endl;
+		return wstring();
+	}
+
+	mt19937 rng(time(nullptr));
+	uniform_int_distribution<size_t> dist(0, words.size() - 1);
+
+	return words[dist(rng)];
+}
+
+
+void startNewGame()
+{
+	string filePath = "words.txt";
+
+	vector<wstring> words = loadWordsFromFile(filePath);
+
+	SecretWord.clear();
+	SecretWord = chooseRandomWord(words);
+	currentRow = 0;
+	currentCell = 0;
+}
+
+wstring GetUserName() 
+{
+	wchar_t username[UNLEN + 1];
+	DWORD username_len = UNLEN + 1;
+
+	if (GetUserName(username, &username_len)) 
+	{
+		return username;
+	}
+	else 
+	{
+		return L"UnknownUser";
+	}
+
+}
+
+void WriteResultToFile(const wstring& secretWord, int attempts, bool success) 
+{
+	string ResultfilePath = "result.txt";
+	wofstream resultFile(ResultfilePath, ios::app);
+	if (!resultFile.is_open()) {
+		cerr << "Error opening results file." << endl;
+		return;
+	}
+
+	resultFile.imbue(locale(resultFile.getloc(), new codecvt_utf8<wchar_t, 0x10ffff, codecvt_mode::little_endian>));
+
+
+	time_t t = time(nullptr);
+	tm localTime;
+	localtime_s(&localTime, &t);
+
+	wchar_t dateTime[100];
+	wcsftime(dateTime, sizeof(dateTime), L"%d.%m.%Y", &localTime);
+
+	wstring username = GetUserName();
+
+	resultFile << secretWord << L": "
+		<< (success ? L"вгадано" : L"не вгадано")
+		<< L", спроб: " << attempts
+		<< L", дата: " << dateTime
+		<< L", користувач: " << username
+		<< endl;
+
+	resultFile.close();
+}
+vector<wstring> LoadResultsFromFile() 
+{
+	string ResultfilePath = "result.txt";
+	wifstream resultFile(ResultfilePath);
+
+	vector<wstring> Resultwords;
+
+	resultFile.imbue(locale(resultFile.getloc(), new codecvt_utf8<wchar_t, 0x10ffff, codecvt_mode::little_endian>));
+
+
+	if (resultFile.is_open()) {
+		wstring ResultWord;
+		while (getline(resultFile, ResultWord)) {
+			Resultwords.push_back(ResultWord);
+		}
+		resultFile.close();
+	}
+	else {
+		cerr << "Error" << ResultfilePath << endl;
+	}
+
+	return Resultwords;
+}
+
+void RestartGame() 
+{
+	for (int row = 0; row < 6; row++) {
+		for (int col = 0; col < 5; col++) {	
+
+			SetWindowText(cells[row][col], L""); 
+		}
+	}
+	EnteredWord.clear();
+	mistakesCount = 0;
+	enteredWordCount = 0;
+	startNewGame();
+	SetFocus(cells[0][0]);
+}
+// Попередній опис функцій
+
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR  CALLBACK DialogProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR  CALLBACK ResultsDialogProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+// Основна програма 
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	MSG msg;
+
+	// Реєстрація класу вікна 
+	MyRegisterClass(hInstance);
+
+	DialogBox(hInstance, MAKEINTRESOURCE(101), NULL, (DLGPROC)DialogProc);
+	// Створення вікна програми
+	if (!	(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+	// Цикл обробки повідомлень
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW ; 		//стиль вікна
+	wcex.lpfnWndProc = (WNDPROC)WndProc; 		//віконна процедура
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance; 			//дескриптор програми
+	wcex.hIcon = LoadIcon(NULL, MAKEINTRESOURCE(IDI_ICON1));	//визначення іконки
+	//wcex.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CROSS));
+	wcex.hbrBackground = GetSysColorBrush(COLOR_WINDOW); //установка фону
+	wcex.lpszMenuName = NULL; 				//визначення меню
+	wcex.lpszClassName = szWindowClass; 		//ім’я класу
+	wcex.hIconSm = LoadIcon(NULL, MAKEINTRESOURCE(IDI_ICON1));
+	
+
+	return RegisterClassEx(&wcex); 			//реєстрація класу вікна
+}
+
+// FUNCTION: InitInstance (HANDLE, int)
+// Створює вікно програми і зберігає дескриптор програми в змінній hInst
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; //зберігає дескриптор додатка в змінній hInst
+	HWND hWnd = CreateWindow(szWindowClass, 	// ім’я класу вікна
+		szTitle, 				// назва програми
+		WS_OVERLAPPEDWINDOW,			// стиль вікна
+		CW_USEDEFAULT, 			// положення по Х	
+		CW_USEDEFAULT,			// положення по Y	
+		CW_USEDEFAULT, 			// розмір по Х
+		CW_USEDEFAULT, 			// розмір по Y
+		NULL, 					// дескриптор батьківського вікна	
+		NULL, 					// дескриптор меню вікна
+		hInstance, 				// дескриптор програми
+		NULL); 				// параметри створення.
+
+	if (!hWnd) 	//Якщо вікно не творилось, функція повертає FALSE
+	{
+		return FALSE;
+	}
+	ShowWindow(hWnd, nCmdShow); 		//Показати вікно
+	UpdateWindow(hWnd); 				//Оновити вікно
+	return TRUE;
+}
+
+// FUNCTION: WndProc (HWND, unsigned, WORD, LONG)
+// Віконна процедура. Приймає і обробляє всі повідомлення, що приходять в додаток
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	return DialogBox(hInst, MAKEINTRESOURCE(101), NULL, DialogProc);
+}
+INT_PTR CALLBACK ResultsDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) 
+{
+	switch (message) 
+	{
+		case WM_INITDIALOG:
+		{
+			vector<wstring> results = LoadResultsFromFile();
+			wstring resultText;
+			for (const wstring& line : results) {
+				resultText += line + L"\r\n";
+			}
+			SetDlgItemText(hDlg, 1093, resultText.c_str());
+			return TRUE;
+			
+		}
+		case WM_COMMAND:
+		{
+			if (LOWORD(wParam) == IDOK )
+			{
+				EndDialog(hDlg, LOWORD(wParam));
+				return TRUE;
+			}
+			break;	
+
+		}
+
+	}
+	return FALSE;
+
+}
+
+INT_PTR CALLBACK DialogProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+
+	case WM_INITDIALOG:
+	{
+		HMENU hMenu = LoadMenu(hInst, MAKEINTRESOURCE(104));
+
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)));
+
+		if (hMenu)
+		{
+			SetMenu(hwnd, hMenu);
+		}
+
+		InitializeEditBoxes(hwnd);
+		ApplyCustomFontToAllEditBoxes(hFont, cells);
+		startNewGame();
+		return TRUE;
+	}
+
+	case WM_SETCURSOR:
+	{
+		SetCursor((HCURSOR)LoadCursor(hInst, MAKEINTRESOURCE(IDC_CURSOR2)));
+		return TRUE;
+	}
+	case WM_COMMAND:
+	{
+		int Id = LOWORD(wParam);
+
+		if (Id >= 1001 && Id <= 1033)
+		{
+			wchar_t buttonText[2];
+			GetWindowText(GetDlgItem(hwnd, Id), buttonText, 2);
+
+			if (currentRow  < 6 && currentCell < 5)
+			{
+				SetWindowText(cells[currentRow][currentCell], buttonText);
+
+				currentCell++;
+				if (currentCell == 5) 
+				{
+					if (Id == 1035) {
+						currentCell = 0;
+						currentRow++;
+
+						if (currentRow < 6) {
+							SetFocus(cells[currentRow][currentCell]);
+						}
+					}
+				}
+			}
+		}
+		else if (Id == 1034)
+		{
+			if (currentCell > 0)
+			{
+				currentCell--;
+				SetWindowText(cells[currentRow][currentCell], L"");
+				SetFocus(cells[currentRow][currentCell]);
+
+			}
+		}
+
+
+		else if (Id == 1035)
+		{
+			bool allCellsCorrect = true;
+			wchar_t enteredWord[5] = { 0 };
+
+			for (int col = 0; col < 5; col++)
+			{
+				wchar_t cellText[2] = { 0 };
+
+				GetWindowText(cells[currentRow][col], cellText, 2);
+
+				enteredWord[col] = cellText[0];
+				if (wcslen(cellText) == 0)
+				{
+					allCellsFilled = false;
+					MessageBox(hwnd, L"Занадто коротко", L"Коротко!", MB_OK);
+					break;
+				}
+				else 
+
+					allCellsFilled = true;
+
+			}
+
+			if (allCellsFilled)
+			{
+
+				wstring entWrd;
+				entWrd.assign(enteredWord, enteredWord + sizeof(enteredWord) / sizeof(wchar_t));
+				EnteredWord.push_back(entWrd);
+				for (int i = 0; i < 6; i++) 
+				{
+					for (int j = 0; j < 5; j++) 
+					{
+
+						if (entWrd[j] != SecretWord[j]) 
+						{
+							mistakesCount++;
+							allCellsCorrect = false;
+							break;
+							
+						}
+
+
+					}
+
+
+				}
+				enteredWordCount++;
+
+				if (enteredWordCount==6 && mistakesCount >=1 )
+				{
+					InvalidateRect(hwnd, NULL, TRUE);
+					MessageBox(hwnd, L"Ви програли:(", L"Програш:(", MB_OK);
+					WriteResultToFile(SecretWord, enteredWordCount, false);
+					RestartGame();
+					SetFocus(cells[0][0]);
+					return TRUE;
+				}
+				if (allCellsCorrect)
+				{
+					InvalidateRect(hwnd, NULL, TRUE);
+					MessageBox(hwnd, L"Вітаю! Ви перемогли!", L"Перемога!", MB_OK);
+					WriteResultToFile(SecretWord,enteredWordCount, true);
+					RestartGame();
+					SetFocus(cells[0][0]);
+					return TRUE;
+
+				}
+				
+				InvalidateRect(hwnd, NULL, TRUE);
+
+				currentRow++;
+				currentCell = 0;
+				if (currentRow < 6)
+				{
+					SetFocus(cells[currentRow][currentCell]);
+				}
+			}
+
+		}
+		else if (Id == 40001)
+		{
+			MessageBox(hwnd, L"КІУКІ-22-7, Довбня К.О.", L"О нас", MB_OK);
+		}
+		else if (Id == 40002)
+		{
+			MessageBox(hwnd,
+				L"Задача гри максимально проста,потрібно вгадати загадане слово. У користувача є шість спроб для того щоб вгадати слово, після шести спроб гра буде завершена.\nВ грі існує три кольори підсвітки літер,ось пояснення до кожного кольору:\nЗелений - літера вгадана правильно і стоїть в правильній позиції\nПомаранчевий - літера вгадана правильна, але стоїть не в правильній позиції\nСірий - літера взагалі не вгадана.", L"Інструкція користувача", 
+				MB_OK);
+		}
+		else if (Id == 40003)
+		{
+			DialogBox(hInst, MAKEINTRESOURCE(105), hwnd, ResultsDialogProc);
+
+		}
+
+		
+		break;
+	}
+
+	case WM_CTLCOLORSTATIC:
+{
+	HDC hdc = (HDC)wParam;
+	HWND hwndCtl = (HWND)lParam;
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (i >= EnteredWord.size())
+			continue;
+		wstring myEntWrd = EnteredWord.at(i);
+		for (int j = 0; j < 5; j++)
+		{
+			if (cells[i][j] == hwndCtl)
+			{
+				if (myEntWrd.at(j) == SecretWord.at(j))
+				{
+					color = RGB(0, 255, 0); //зелений
+				}
+				else
+				{
+					auto pos = SecretWord.find(myEntWrd.at(j));
+					if (pos != string::npos)
+					{
+						color = RGB(255, 165, 0);//помаранчевий
+					}
+					else
+						color = RGB(128, 128, 128);//сірий
+				}
+					
+
+				return (LRESULT)CreateSolidBrush(color);
+
+			}
+
+		}
+
+	}
+	break;
+	}
+	
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return FALSE;
+}
---
+++ Cursova/resource1.h
@@ -0,0 +1,85 @@
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       104
+#define IDD_DIALOG2                     105
+#define IDC_CURSOR2                     113
+#define IDI_ICON1                       114
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
+#define IDC_BUTTON4                     1004
+#define IDC_BUTTON5                     1005
+#define IDC_BUTTON6                     1006
+#define IDC_BUTTON7                     1007
+#define IDC_BUTTON8                     1008
+#define IDC_BUTTON9                     1009
+#define IDC_BUTTON10                    1010
+#define IDC_BUTTON11                    1011
+#define IDC_BUTTON12                    1012
+#define IDC_BUTTON13                    1013
+#define IDC_BUTTON14                    1014
+#define IDC_BUTTON15                    1015
+#define IDC_BUTTON16                    1016
+#define IDC_BUTTON17                    1017
+#define IDC_BUTTON18                    1018
+#define IDC_BUTTON19                    1019
+#define IDC_BUTTON20                    1020
+#define IDC_BUTTON21                    1021
+#define IDC_BUTTON22                    1022
+#define IDC_BUTTON23                    1023
+#define IDC_BUTTON24                    1024
+#define IDC_BUTTON33                    1025
+#define IDC_BUTTON26                    1026
+#define IDC_BUTTON27                    1027
+#define IDC_BUTTON28                    1028
+#define IDC_BUTTON29                    1029
+#define IDC_BUTTON30                    1030
+#define IDC_BUTTON31                    1031
+#define IDC_BUTTON32                    1032
+#define IDC_BUTTON34                    1033
+#define IDC_BUTTON25                    1034
+#define IDC_BUTTON35                    1035
+#define IDC_EDIT1                       1061
+#define IDC_EDIT2                       1062
+#define IDC_EDIT3                       1063
+#define IDC_EDIT4                       1064
+#define IDC_EDIT5                       1065
+#define IDC_EDIT6                       1066
+#define IDC_EDIT7                       1067
+#define IDC_EDIT8                       1068
+#define IDC_EDIT9                       1069
+#define IDC_EDIT10                      1070
+#define IDC_EDIT11                      1071
+#define IDC_EDIT12                      1072
+#define IDC_EDIT13                      1073
+#define IDC_EDIT14                      1074
+#define IDC_EDIT15                      1075
+#define IDC_EDIT16                      1076
+#define IDC_EDIT17                      1077
+#define IDC_EDIT18                      1078
+#define IDC_EDIT19                      1079
+#define IDC_EDIT20                      1080
+#define IDC_EDIT21                      1081
+#define IDC_EDIT22                      1082
+#define IDC_EDIT23                      1083
+#define IDC_EDIT24                      1084
+#define IDC_EDIT25                      1085
+#define IDC_EDIT26                      1086
+#define IDC_EDIT27                      1087
+#define IDC_EDIT28                      1088
+#define IDC_EDIT29                      1089
+#define IDC_EDIT30                      1090
+#define IDC_EDITRESULT1                 1093
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1095
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
